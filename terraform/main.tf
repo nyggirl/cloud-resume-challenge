cd3cb1fddf5d46b4ab5f1de0b0ad6817f6bafe3d
fuzzycarrot <fujinghan8@gmail.com>
2026-07-11 19:24:43 -0400

    feat: add CloudFront CDN with private S3 origin

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,10 +17,10 @@ resource "aws_s3_bucket_website_configuration" "website" {
 resource "aws_s3_bucket_public_access_block" "website" {
   bucket = aws_s3_bucket.website.id
 
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_s3_bucket_policy" "website" {
@@ -31,18 +31,24 @@ resource "aws_s3_bucket_policy" "website" {
 
     Statement = [
       {
-        Sid       = "PublicReadForWebsite"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.website.arn}/*"
+        Sid    = "AllowCloudFrontServicePrincipal"
+        Effect = "Allow"
+
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        }
+
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.website.arn}/*"
+
+        Condition = {
+          StringEquals = {
+            "AWS:SourceArn" = aws_cloudfront_distribution.website.arn
+          }
+        }
       }
     ]
   })
-
-  depends_on = [
-    aws_s3_bucket_public_access_block.website
-  ]
 }
 
 resource "aws_s3_object" "index" {
@@ -70,4 +76,63 @@ resource "aws_s3_object" "script" {
   content_type = "application/javascript"
 
   etag = filemd5("${path.module}/../frontend/script.js")
+}
+
+resource "aws_cloudfront_origin_access_control" "website" {
+  name                              = "cloud-resume-oac"
+  description                       = "Origin Access Control for the Cloud Resume Challenge"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "website" {
+  enabled             = true
+  default_root_object = "index.html"
+  comment             = "Cloud Resume Challenge distribution"
+  price_class         = "PriceClass_100"
+
+  origin {
+    domain_name              = aws_s3_bucket.website.bucket_regional_domain_name
+    origin_id                = "s3-cloud-resume-origin"
+    origin_access_control_id = aws_cloudfront_origin_access_control.website.id
+  }
+
+  default_cache_behavior {
+    target_origin_id       = "s3-cloud-resume-origin"
+    viewer_protocol_policy = "redirect-to-https"
+    compress               = true
+
+    allowed_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    cached_methods = [
+      "GET",
+      "HEAD"
+    ]
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl     = 0
+    default_ttl = 3600
+    max_ttl     = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -12,3 +12,18 @@ output "website_url" {
   description = "Public URL for the S3-hosted website."
   value       = "http://${aws_s3_bucket_website_configuration.website.website_endpoint}"
 }
+
+output "cloudfront_distribution_id" {
+  description = "ID of the CloudFront distribution."
+  value       = aws_cloudfront_distribution.website.id
+}
+
+output "cloudfront_domain_name" {
+  description = "Domain name assigned to the CloudFront distribution."
+  value       = aws_cloudfront_distribution.website.domain_name
+}
+
+output "cloudfront_url" {
+  description = "HTTPS URL for the CloudFront-hosted website."
+  value       = "https://${aws_cloudfront_distribution.website.domain_name}"
+}
